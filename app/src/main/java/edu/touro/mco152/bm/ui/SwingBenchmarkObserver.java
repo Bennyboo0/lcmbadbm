9b75b939205934036b509f3ccf14af23fc655fb5
Benjamin Yazdi <byazdi13@gmail.com>
2026-03-19 16:01:46 -0400

    changed a comment

--- app/src/main/java/edu/touro/mco152/bm/ui/SwingBenchmarkObserver.java
+++ app/src/main/java/edu/touro/mco152/bm/ui/SwingBenchmarkObserver.java
@@ -17,7 +17,7 @@ public class SwingBenchmarkObserver implements BenchmarkObserver {
 
     }
 
-    /** Allows the worker to be set after construction, resolving the circular dependency. */
+    /** this method allows the worker to be set after construction, resolving the circular dependency. */
     public void setWorker(SwingWorkerWrapper worker) {
         this.worker = worker;
     }
