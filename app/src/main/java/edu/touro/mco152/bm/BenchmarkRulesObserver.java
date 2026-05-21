73af05263919d3e145e18602d5a4bc1a7b12ed97
Benjamin Yazdi <byazdi13@gmail.com>
2026-05-21 16:59:33 -0400

    iteration 1 for functionality w/o slack

--- app/src/main/java/edu/touro/mco152/bm/BenchmarkRulesObserver.java
+++ app/src/main/java/edu/touro/mco152/bm/BenchmarkRulesObserver.java
@@ -8,20 +8,24 @@ import edu.touro.mco152.bm.persist.DiskRun;
  * As you can see, we only check 1 rule, which if the read benchmark max time exceeds average by > 3%
  */
 public class BenchmarkRulesObserver implements BenchmarkRunObserver {
-    private final SlackManager slackManager;
+    //private final SlackManager slackManager;
 
     public BenchmarkRulesObserver() {
-        this.slackManager = new SlackManager("BadBM");
+        //this.slackManager = new SlackManager("BadBM");
     }
+
 
     @Override
     public void addRun(DiskRun run) {
-        if (run.getRunType() == DiskRun.IOMode.READ) {
+        if (run.getIoMode() == DiskRun.IOMode.READ) {
             // If max time > 3% higher than average time
-            if (run.getMax() > (run.getAvg() * 1.03)) {
+            double maxTime = Double.parseDouble(run.getMax());
+            double avgTime = Double.parseDouble(run.getAvg()); //need to cast these to doubles bc of the comparison
+
+            if (maxTime > (avgTime * 1.03)) {
                 String msg = String.format(":warning: Read Max (%.2f ms) exceeded 3%% of Avg (%.2f ms)!",
-                        run.getMax(), run.getAvg());
-                slack.postMsg2OurChannel(msg);
+                        maxTime, avgTime);
+                //slack.postMsg2OurChannel(msg);
             }
         }
         //and so, like the hw requests for, future rules can easily be appended right here without changing core benchmark code
--- app/src/main/java/edu/touro/mco152/bm/DiskWorker.java
+++ app/src/main/java/edu/touro/mco152/bm/DiskWorker.java
@@ -1,11 +1,13 @@
 package edu.touro.mco152.bm;
 import edu.touro.mco152.bm.commands.BenchmarkExecutor;
+import edu.touro.mco152.bm.persist.DatabasePersistenceObserver;
 import edu.touro.mco152.bm.ui.Gui;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static edu.touro.mco152.bm.App.*;
 import edu.touro.mco152.bm.commands.WriteBM;
 import edu.touro.mco152.bm.commands.ReadBM;
+import edu.touro.mco152.bm.ui.GuiRunPanelObserver;
 
 /**
  * Contains the core disk benchmark logic, decoupled from any UI or threading framework.
@@ -48,18 +50,36 @@ public class DiskWorker {
 
         BenchmarkExecutor executor = new BenchmarkExecutor();
 
+        DatabasePersistenceObserver dbObserver = new DatabasePersistenceObserver();
+        GuiRunPanelObserver guiObserver = new GuiRunPanelObserver(Gui.runPanel);
+        BenchmarkRulesObserver rulesObserver = new BenchmarkRulesObserver();
+
         if (App.writeTest) {
-            executor.addCommand(new WriteBM(
+            WriteBM writeCmd = new WriteBM(
                     runner, observer,
                     App.numOfMarks, App.numOfBlocks, App.blockSizeKb, App.blockSequence, App.multiFile, App.writeSyncEnable
-            ));
+            );
+
+            // Register all three structural observers to the Write command
+            writeCmd.registerObserver(dbObserver);
+            writeCmd.registerObserver(guiObserver);
+            writeCmd.registerObserver(rulesObserver);
+
+            executor.addCommand(writeCmd);
         }
 
         if (App.readTest) {
-            executor.addCommand(new ReadBM(
+            ReadBM readCmd = new ReadBM(
                     runner, observer,
                     App.numOfMarks, App.numOfBlocks, App.blockSizeKb, App.blockSequence, App.multiFile
-            ));
+            );
+
+            // Register all three structural observers to the Read command
+            readCmd.registerObserver(dbObserver);
+            readCmd.registerObserver(guiObserver);
+            readCmd.registerObserver(rulesObserver);
+
+            executor.addCommand(readCmd);
         }
 
         executor.executeAll();
