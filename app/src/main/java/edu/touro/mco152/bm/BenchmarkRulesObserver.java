d0abaedda3db5c4b9b1f3b3bc9b3dba4825de390
Benjamin Yazdi <byazdi13@gmail.com>
2026-05-21 18:12:40 -0400

    this implement observer Pattern with slack notification

--- app/src/main/java/edu/touro/mco152/bm/BenchmarkRulesObserver.java
+++ app/src/main/java/edu/touro/mco152/bm/BenchmarkRulesObserver.java
@@ -1,16 +1,17 @@
 package edu.touro.mco152.bm;
 
 import edu.touro.mco152.bm.persist.DiskRun;
+import edu.touro.mco152.bm.externalsys.SlackManager;
 
 /**
  * This class is to follow the advice in 7c on the hw doc. It allows for all rules to be put in there.
  * As you can see, we only check 1 rule, which if the read benchmark max time exceeds average by > 3%
  */
 public class BenchmarkRulesObserver implements BenchmarkRunObserver {
-    //private final SlackManager slackManager;
+    private final SlackManager slackManager;
 
     public BenchmarkRulesObserver() {
-        //this.slackManager = new SlackManager("BadBM");
+        this.slackManager = new SlackManager("BadBM");
     }
 
 
@@ -25,7 +26,7 @@ public class BenchmarkRulesObserver implements BenchmarkRunObserver {
             if (avgTime > 0 && maxTime > (avgTime * 1.03)) { //we have the first condition so that this doesn't do off when a run is uninitialized
                 String msg = String.format(":warning: Read Max (%.2f ms) exceeded 3%% of Avg (%.2f ms)!",
                         maxTime, avgTime);
-                //slack.postMsg2OurChannel(msg);
+                slackManager.postMsg2OurChannel(msg);
             }
         }
         //and so, like the hw requests for, future rules can easily be appended right here without changing core benchmark code
